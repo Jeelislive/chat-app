f922acb3fd44f3eeda0bb99be13b96b92967ae92
Jeel rupareliya <jeelrupareliya255@gmail.com>
2024-09-13 13:26:25 +0530

    .

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,12 +3,11 @@ import { ErrorHandler } from "../utils/utility.js";
 import { TryCatch } from "./error.js";
 import { CHATAPPTOKEN } from "../constants/config.js";
 import { User } from "../models/user.model.js";
-import env from "dotenv";
-env.config();
+
 
 const isAuthenticated = TryCatch(async (req, res, next) => {
    const token = req.cookies[CHATAPPTOKEN];
-   
+   console.log(token);
    if (!token) return next(new ErrorHandler("Please login to access this route", 401));
 
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
